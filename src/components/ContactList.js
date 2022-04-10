d602ff1de176ca8a9309f57fb70c9f9d6c96b9bd
vikthour01 <vikthour01@gmail.com>
2022-04-09 22:32:35 -1200

    fifth commit

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,10 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState} from "react";
 import { Link } from "react-router-dom";
 import ContactCard from "./ContactCard";
 import api from "../api/data";
 import "./css/contactList.css";
 
 const ContactList = ({ contacts, setContacts }) => {
+  const [isLoading, setIsLoading] = useState(true)
+
   // Gets the data from API
   const retrieveData = async () => {
     const response = await api.get("/users");
@@ -13,16 +15,24 @@ const ContactList = ({ contacts, setContacts }) => {
 
   ///this is for getting data from api
   useEffect(() => {
+    setIsLoading(true)
     const getAllData = async () => {
       const allData = await retrieveData();
+      setIsLoading(false)
       if (allData) setContacts(allData);
     };
     getAllData();
-  }, [contacts]);
+  },[]);
 
   const renderedContactList = contacts.map((contact) => {
     return <ContactCard contact={contact} onClickHandler key={contact.id} />;
   });
+
+  // if(isLoading){
+  //   return(
+  //     <div>Loading</div>
+  //   )
+  // }
 
   return (
     <div className="AppContainer">
@@ -36,7 +46,7 @@ const ContactList = ({ contacts, setContacts }) => {
         </Link>
       </div>
 
-      {renderedContactList}
+      {isLoading ? "loading... " : renderedContactList}
     </div>
   );
 };
